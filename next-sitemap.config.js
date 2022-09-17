4d9d3b3e11df2563dffd53edc1bb668d731b89c4
Raj Kumar <rajkmr.rkp@gmail.com>
2022-09-17 14:34:17 +0530

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://codecrux.dev',
+    generateRobotsTxt: true, // (optional)
+    exclude: [`/admin*`, '/preview', '/carousel', '/home']
+    // ...other options
+}
